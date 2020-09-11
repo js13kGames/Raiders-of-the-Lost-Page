2524b0a9013bf66182d3debd3627636e47d2083e
Roberto Serra <obi.serra@gmail.com>
2020-09-11 09:48:01 +0200

    concat during build

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,10 @@ const clean = require("gulp-clean");
 const replace = require("gulp-replace");
 const htmlmin = require('gulp-htmlmin');
 const cleanCSS = require('gulp-clean-css');
+const through = require('through2')
+const concat = require('gulp-concat');
+
+const { StringDecoder } = require('string_decoder');
 
 const sass = require("gulp-sass");
 sass.compiler = require("node-sass");
@@ -28,8 +32,14 @@ function compileSass() {
 }
 
 function compress() {
-  return src([paths.js.dev + "/*.js"])
-    .pipe(
+  return src([paths.js.dev + "/game_maps.js",paths.js.dev + "/game_main.js", paths.js.dev + "/!(game_maps, game_main)*.js"])
+  .pipe(concat('index.js'))
+  .pipe(through.obj((file, enc, cb) => {
+   const content = file.contents.toString().split("\r\n").map (c => c.replace(/\n/, "").replace(/\r/, "").replace(/^export (default)?\s?/g, "").replace(/^import .*/g, ""))
+    file.contents =  Buffer.from(content.join("\n"), 'utf8')
+  cb(null,file );      
+  }))   
+   .pipe(
       minify({
         ext: {
           min: ".js",
